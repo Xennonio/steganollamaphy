37267bd05e0b5ea39833e5eb4cfa1688cb95d59b
unknown <ani22dme@gmail.com>
2023-08-27 08:50:25 -0300

    comments added

--- app.py
+++ app.py
@@ -1,30 +1,41 @@
+# Import necessary libraries
 from flask import Flask, render_template, request, redirect, session, jsonify
 from PIL import Image
 
+# Import cryptography functions from another file
 from llamanographer import encryptimg, decryptimg, text_to_binary, image_scale
 
+# Create a Flask web application
 app = Flask(__name__, static_url_path='/static')
 
+# Define a route for the root URL '/'
 @app.route('/')
 def index():
     return render_template('layout.html')
 
-
+# Define a route for handling POST requests to '/get_image_size'
 @app.route('/get_image_size', methods=['POST'])
 def get_image_size():
+    # Check if a file named 'img' is included in the POST request
     if request.files['img']:
         image = Image.open(request.files['img'])
         image_size = image.size[0] * image.size[1]
+
+        # Return the image size as a JSON response
         return jsonify({'image_size': image_size})
+    
+    # If no 'img' file is found in the request, return None as the image size
     return jsonify({'image_size': None})
 
-
+# Define a route for handling both GET and POST requests at '/encrypt'
 @app.route('/encrypt', methods = ['GET', 'POST'])
 def encrypt():
     # Initialize image_size with a default value (e.g., None) if it doesn't exist in the session
     image_size = session.get('image_size', None)
 
+    # Check if the request method is POST
     if request.method == 'POST':
+        # Check if the necessary files are included in the POST request
         if not request.files['img']:
             print('error')
             redirect('/')
@@ -35,34 +46,40 @@ def encrypt():
             print('error')
             redirect('/')
 
+        # Open the files from the request
         image = Image.open(request.files['img'])
         message = request.form.get('msg')
         filename = request.form.get('filename')
 
+        # Convert the message to binary and encrypt image with the binary message
         binary_message = text_to_binary(message)
         encrypt_check = encryptimg(image, filename, binary_message)
 
         image_size = image.size[0] * image.size[1]
 
+        # Replace spaces in the filename with '%20' for URLs
         urlname = ''
-
         for character in filename:
             if character == ' ':
                 urlname += '%20'
             else:
                 urlname += character
 
+        # Check the result of the encryption
         if encrypt_check:
             return render_template('encrypted.html', name = filename, url = urlname)
         else:
-            return redirect('/')
+            return redirect('/') # Redirect the user to the root URL in case of encryption failure
     else:
         return render_template('encrypt.html', image_size=image_size)
 
-
+# Define a route for handling both GET and POST requests at '/decrypt'
 @app.route('/decrypt', methods = ['GET', 'POST'])
 def decrypt():
+
+    # Check if the request method is POST
     if request.method == 'POST':
+        # Check if the necessary files are included in the POST request
         if not request.files['img']:
             print('error')
             redirect('/')
@@ -70,12 +87,16 @@ def decrypt():
             print('error')
             redirect('/')
 
+        # Open the files from the request
         image = Image.open(request.files['img'])
         textsize = int(request.form.get('textsize'))
         
+        # Decrypt the image and obtain a temporary message
         tmpmessage = decryptimg(image, textsize)
+        # Initialize an empty string to store the decrypted message in characters
         decrypted_message = ''
 
+        # Convert the list of integers in 'tmpmessage' to characters and concatenate them
         for letter in tmpmessage:
             decrypted_message += chr(letter)
 
--- llamanographer.py
+++ llamanographer.py
@@ -1,57 +1,74 @@
 def encryptimg(image, name, message):
+    # Check if the length of the message fits the image
     if len(message) > 3 * image.size[0] * image.size[1]:
         print("text too big for the image")
         return False
     else:
-        counter = 0
-        pixels = image.load()
+        counter = 0  # Initialize a counter to keep track of message bits processed
+        pixels = image.load() # Load the pixel data of the image
+
+        # Loop through the image pixels
         for i in range(image.size[0]):
             for j in range(image.size[1]):
-                tpx = list(pixels[i, j])
+                tpx = list(pixels[i, j]) # Get the RGB values of the current pixel
+
+                # Loop through the RGB channels (red, green, blue)
                 for k in range(3):
-                    '''
-                    if message[counter] % 2 == 0 and tpx[i] % 2 == 1:
-                        tpx[i] -= 1
-                    if message[counter] % 2 == 1 and tpx[i] % 2 == 0:
-                        tpx[i] += 1
-                    '''
+                    # Check if the current message bit is different from the least significant bit of the pixel channel
                     if message[counter] != tpx[k] % 2:
-                        tpx[k] += 2*message[counter] - 1
-                    counter += 1
+                        # Modify the pixel channel to hide the message bit
+                        tpx[k] += 2*message[counter] - 1 # Ensures the pixel channel is even or odd depending on the message bit
+                    counter += 1 # Move to the next message bit
+
+                    # Check if all message bits have been processed
                     if counter == len(message):
-                        pixels[i, j] = tuple(tpx)
-                        image.save('static/' + name + '.jpg', format = "PNG")
-                        return True
-                pixels[i, j] = tuple(tpx)
+                        pixels[i, j] = tuple(tpx) # Update the pixel with modified values
+                        image.save('static/' + name + '.jpg', format = "PNG") # Save the modified image
+                        return True # Return True to indicate successful encryption
+                pixels[i, j] = tuple(tpx) # Update the pixel with modified values
 
 
 def decryptimg(image, size):
-    message = []
-    tmp = []
-    counter = 0
-    pixels = image.load()
+    # Initialize empty var to store:
+    message = [] # decrypted msg
+    tmp = [] # each unicode character in binary
+    counter = 0 # keep track of processed bits
+    pixels = image.load() # Load the pixel data of the image
+
+    # Loop through the image pixels
     for i in range(image.size[0]):
         for j in range(image.size[1]):
+
+            # Iterate through the RGB channels (red, green, blue)
             for k in range(3):
+                # Get the least significant bit of the pixel channel
                 tmp.append(pixels[i, j][k] % 2)
                 counter += 1
+
+                # Check if a byte (unicode character) have been collected
                 if counter % 8 == 0:
                     tmpstr = ''
                     for bit in tmp:
-                        tmpstr += str(bit)
-                    message.append(int(tmpstr, base = 2))
-                    tmp = []
+                        tmpstr += str(bit) # Convert the byte to string
+                    message.append(int(tmpstr, base = 2)) # Convert the byte to binary
+                    tmp = [] # Reset the temporary byte list
+
+                # Check if the total number of bits processed equals the expected size
                 if counter == 8*size:
-                    return message
+                    return message # Return the decrypted message as a list of bytes
     return message
 
 
 def text_to_binary(message):
-    binary_message = []
+    binary_message = [] # Initialize an empty list to store each bit of the message
+    
+    # Iterate through each character in the input message
     for letter in message:
-        bin = format(ord(letter), '08b')
+        bin = format(ord(letter), '08b') # Convert the character to its 8-bit binary representation
+
+        # Iterate through each bit in the binary representation
         for bit in bin:
-            binary_message.append(int(bit))
+            binary_message.append(int(bit)) # store in binary message list
     return binary_message
 
 
